fed08a9e6ff50cb5bd41c16e68470b350f5c9291
Ishmil <ismail.assam2008@gmail.com>
2026-03-01 23:52:23 +0000

    api key

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
 //  It is already in .gitignore if you used the provided one.
 // ============================================================
 
-export const MISTRAL_API_KEY = "your_actual_key_here";
+export const MISTRAL_API_KEY = "324E757F-23A8-4E86-8A4F-FA0433F6B25A";
