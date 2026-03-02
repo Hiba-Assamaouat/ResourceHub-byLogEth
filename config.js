3b24436463a975e5b1a196a9a21352d464ec4dd6
Ishmil <74981404+Ishmil62@users.noreply.github.com>
2026-03-02 00:08:32 +0000

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// ============================================================
-//  config.js  —  API keys and secrets
-//  !! ADD THIS FILE TO YOUR .gitignore !!
-//  It is already in .gitignore if you used the provided one.
-// ============================================================
-
-export const MISTRAL_API_KEY = "IRb4FdAzhZrmskgtmRgLjcFiv0c8McdF";
